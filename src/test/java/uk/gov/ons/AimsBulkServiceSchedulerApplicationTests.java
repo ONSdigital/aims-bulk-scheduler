5983c96b725d770f54575cb4a7b440713a7c2aae
steve-thorne-ONS <steven.thorne@ons.gov.uk>
2022-09-28 11:22:58 +0100

    Fix tests.

--- src/test/java/uk/gov/ons/AimsBulkServiceSchedulerApplicationTests.java
+++ src/test/java/uk/gov/ons/AimsBulkServiceSchedulerApplicationTests.java
@@ -2,8 +2,10 @@ package uk.gov.ons;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AimsBulkServiceSchedulerApplicationTests {
 
 	@Test
